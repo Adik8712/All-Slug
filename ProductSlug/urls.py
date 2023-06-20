59b9806f0ddab23fa81d4ad156d2d8227deebc0e
Adil8712 <taukelevadil@gmail.com>
2023-06-20 20:58:36 +0600

    All Slug V2

--- ProductSlug/urls.py
+++ ProductSlug/urls.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import routers
+from main.api import CategoryViewSet, ProductViewSet
+
+router = routers.DefaultRouter()
+router.register(r'category', CategoryViewSet)
+router.register(r'product', ProductViewSet)
 
 urlpatterns = [
+    path('api/v1/', include(router.urls)),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from .views import IndexView, ProductDetailView
+from .api import ProductViewSet, CategoryViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+router.register('api/categoty', CategoryViewSet, basename='category')
+router.register('api/product', ProductViewSet, basename='product')
+
 
 urlpatterns = [
     path('', IndexView, name='index'),
     path('product/<slug:slug>/', ProductDetailView, name='product_detail'),
 ]
+
+urlpatterns += router.urls
